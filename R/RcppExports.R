ec838daf5278e4bf7ad980b82d004c1274b549cb
Yuanming Zhang <soyzhang@mail.hzau.edu.cn>
2020-10-08 09:50:02 +0000

    version 4.0.2

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -2,7 +2,8 @@
 # Generator token: 10BE3573-1514-4C36-9D1C-5A225CD40393
 
 multiplication_speed <- function(A, B) {
-    .Call(`_mrMLM_multiplication_speed`, A, B)
+    .Call('_mrMLM_multiplication_speed', PACKAGE = 'mrMLM', A, B)
 }
+
 
 
--- src/multiplication_speed.cpp
+++ src/multiplication_speed.cpp
@@ -1,8 +1,5 @@
-// [[Rcpp::depends(RcppArmadillo, RcppEigen)]]
-
-#include <RcppArmadillo.h>
+// [[Rcpp::depends(RcppEigen)]]
 #include <RcppEigen.h>
-
 // [[Rcpp::export]]
 SEXP multiplication_speed(const Eigen::Map<Eigen::MatrixXd> A, Eigen::Map<Eigen::MatrixXd> B){
   Eigen::MatrixXd C = A * B;
